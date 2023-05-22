8259f59f5e833c4a65b1801b61f14e00e1485505
Joao Vieira <netojoao599@gmail.com>
2023-05-22 16:38:13 -0300

    Iniciando projeto

---
+++ src/main/java/com/gunpla/joao/GunplaApplication.java
@@ -0,0 +1,13 @@
+package com.gunpla.joao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GunplaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GunplaApplication.class, args);
+	}
+
+}
