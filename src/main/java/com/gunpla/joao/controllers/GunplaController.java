54266e23f22a6a707732e1df271f9f944f8a4ed3
Joao Vieira <netojoao599@gmail.com>
2023-05-23 14:46:13 -0300

    enviando e recebendo json

---
+++ src/main/java/com/gunpla/joao/controllers/GunplaController.java
@@ -0,0 +1,17 @@
+package com.gunpla.joao.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/gunplas")
+public class GunplaController {
+
+	@PostMapping
+	public void cadastrar(@RequestBody String json) {
+		
+		System.out.println(json);
+	}
+}
