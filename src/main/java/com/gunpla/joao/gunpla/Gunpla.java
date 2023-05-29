d5ec0a293f70f65a4c8f63dedb42cbcfd5e36234
Joao Vieira <netojoao599@gmail.com>
2023-05-29 11:49:19 -0300

    Removendo lombok

--- src/main/java/com/gunpla/joao/gunpla/Gunpla.java
+++ src/main/java/com/gunpla/joao/gunpla/Gunpla.java
@@ -1,5 +1,7 @@
 package com.gunpla.joao.gunpla;
 
+import java.util.Objects;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -7,19 +9,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity(name = "gunpla")
 @Table(name = "tb_gunplas")
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@EqualsAndHashCode(of = "id")
 public class Gunpla {
 
 	@Id
@@ -38,5 +30,94 @@ public class Gunpla {
 	private Modelo modelo;
 	
 	@Enumerated(EnumType.STRING)
-	private Marca marca; 
+	private Marca marca;
+	
+	
+	public Gunpla() {
+		
+	}
+
+	public Gunpla(Long id, String nome, Serie serie, String unidade, String quantidade, Modelo modelo, Marca marca) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.serie = serie;
+		this.unidade = unidade;
+		this.quantidade = quantidade;
+		this.modelo = modelo;
+		this.marca = marca;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Serie getSerie() {
+		return serie;
+	}
+
+	public void setSerie(Serie serie) {
+		this.serie = serie;
+	}
+
+	public String getUnidade() {
+		return unidade;
+	}
+
+	public void setUnidade(String unidade) {
+		this.unidade = unidade;
+	}
+
+	public String getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(String quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public Modelo getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(Modelo modelo) {
+		this.modelo = modelo;
+	}
+
+	public Marca getMarca() {
+		return marca;
+	}
+
+	public void setMarca(Marca marca) {
+		this.marca = marca;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Gunpla other = (Gunpla) obj;
+		return Objects.equals(id, other.id);
+	} 
 }
